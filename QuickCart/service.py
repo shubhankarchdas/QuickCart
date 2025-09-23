0ed0c519dd2de75c3af6e993c83c62a4ee92965f
shubhankarchdas <shubhankarchdas@gmail.com>
2025-09-23 16:14:44 +0530

    Add product rating display to home page and update product retrieval logic

--- QuickCart/service.py
+++ QuickCart/service.py
@@ -2,7 +2,7 @@ from store.models import Product
 
 
 def get_available_products():
-    return Product.objects.filter(is_available=True).order_by('id')
+    return Product.objects.filter(is_available=True).order_by('-created_at')
 
 def get_products_by_category(category):
     return get_available_products().filter(category=category)
--- QuickCart/views.py
+++ QuickCart/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from store.models import ReviewRating,Product
 from .service import get_available_products
 
 # Create your views here.
 def home(request):
     products = get_available_products()
+    
+    # Get the reviews
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
     context = {
         'products': products,
+        'reviews' : reviews,
     }
     return render(request, 'home.html', context)
 
