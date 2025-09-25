a771f72de1d0d1eec7cc059eef80598d10b82f18
shubhankarchdas <shubhankarchdas@gmail.com>
2025-09-25 22:02:55 +0530

    Add hero section with carousel and related styles, scripts, and images

---
+++ static/js/home.js
@@ -0,0 +1,18 @@
+    // carousal
+    document.addEventListener('DOMContentLoaded', function () {
+        var swiper = new Swiper(".swiper", {
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    });
